c13fb89ed96e924b5ff96bc62fd29fda0a9dc0cb
Hiroaki Honda <d@damin.dev>
2025-04-19 15:52:12 +0900

    fix: Fix to use HSR

--- rng/dataset/config.py
+++ rng/dataset/config.py
@@ -3,22 +3,25 @@ import logging
 import numpy as np
 import open3d as o3d
 
-camera = 'realsense'
-logging.warn(f'Using {camera} camera now!')
+camera = "realsense"
+logging.warn(f"Using {camera} camera now!")
 
 
 def get_camera_intrinsic(camera=camera):
-    if camera == 'kinect':
+    if camera == "kinect":
         intrinsics = np.array(
-            [[631.55, 0, 638.43], [0, 631.21, 366.50], [0, 0, 1]],
-            dtype=np.float32)
-    elif camera == 'realsense':
+            [[631.55, 0, 638.43], [0, 631.21, 366.50], [0, 0, 1]], dtype=np.float32
+        )
+    elif camera == "realsense":
         intrinsics = np.array(
-            [[927.17, 0, 651.32], [0, 927.37, 349.62], [0, 0, 1]],
-            dtype=np.float32)
+            [
+                [538.17646753, 0, 321.72223033],
+                [0, 538.23831376, 238.63675485],
+                [0, 0, 1.0],
+            ]
+        )
     else:
-        raise ValueError(
-            'Camera format must be either "kinect" or "realsense".')
+        raise ValueError('Camera format must be either "kinect" or "realsense".')
     return intrinsics
 
 
